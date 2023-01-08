efcacfd0e2858226f09f9f0d35bd0bb644bae2ea
Elijah <chilesheelijah01@gmail.com>
2023-01-08 17:11:33 +0200

    Fetched data to react app from Express backend

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 
 // GET all workouts
-router.get("/", getWorkouts)
+router.get("/data", getWorkouts)
 
 // GET a single workout
 router.get("/:id", getSingleWorkout)
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,40 @@
+import { useEffect, useState } from "react";
+
+// Components
+import WorkoutDetails from "../components/WorkoutDetails";
+
 const Home = () => {
+
+    const [workouts, setWorkouts] = useState(null)
+
+    // Fetch workouts with UE Hook
+    useEffect(() => {
+
+        const fetchWorkouts = async () => {
+            const response = await fetch('/data')
+
+            // Parse JSON from response
+            const json = await response.json()
+
+            // Check if response is ok
+            if(response.ok) {
+                setWorkouts(json)
+            }
+        }
+
+        fetchWorkouts()
+
+    }, [])
+
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="workouts">
+                {workouts && workouts.map((workout) => (
+                    <WorkoutDetails key={workout._id} workout={workout} />
+                    // <p key={workout._id}> {workout.title}</p>
+                    
+                ))}
+            </div>
         </div>
     )
 }
